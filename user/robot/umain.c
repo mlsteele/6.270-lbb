5aa0e6a4fcc886de70682e11105f8d55cab5f84c
Jessica Kenney <seveneightn9ne@gmail.com>
2013-01-11 22:23:41 -0500

    add encoder_utils.h: encoder_distance() and encoder_speed()

--- user/robot/umain.c
+++ user/robot/umain.c
@@ -1,5 +1,9 @@
+
+#define ENCODER_PIN 25
+
 // Include headers from OS
 #include <joyos.h>
+#include <encoder_utils.h>
 
 // usetup is called during the calibration period. 
 int usetup (void) {
@@ -8,7 +12,8 @@ int usetup (void) {
 
 // Entry point to contestant code.
 int umain (void) {
-
+	printf("\nDistance: %d", encoder_distance());
+	printf("\nSpeed: %d", encoder_speed());
     // YOUR CODE GOES HERE
 
     // Will never return, but the compiler complains without a return

de13f77b672f74c209c1e45479e8612304a21b8c
Jessica Kenney <seveneightn9ne@gmail.com>
2013-01-13 16:46:20 -0500

    Added VPS utils

--- user/robot/umain.c
+++ user/robot/umain.c
@@ -4,19 +4,17 @@
 // Include headers from OS
 #include <joyos.h>
 #include <encoder_utils.h>
+#include <vps_utils.h>
 
 // usetup is called during the calibration period. 
 int usetup (void) {
+	extern volatile uint8_t robot_id;
+  	robot_id = 8;
     return 0;
 }
 
 // Entry point to contestant code.
 int umain (void) {
-	printf("\nDistance: %d", encoder_distance());
-	printf("\nSpeed: %d", encoder_speed());
-    // YOUR CODE GOES HERE
-
-    // Will never return, but the compiler complains without a return
-    // statement.
+	
     return 0;
 }

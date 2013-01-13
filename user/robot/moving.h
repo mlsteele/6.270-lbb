b7464116a38c8bb19811b4a40779e4473a204972
Nicole Glabinski <nicolekg@mit.edu>
2013-01-13 18:57:33 -0500

    add dumb moving

--- user/robot/moving.h
+++ user/robot/moving.h
@@ -1,6 +1,8 @@
 #include <math.h>
 
-struct Point current_loc;
+#define SPEED 150
+
+Point current_loc;
 float current_angle = 0; //probably want to set it to a different value
 
 void set_starting_loc(void) {
@@ -42,7 +44,7 @@ int robot_moving(void) {
 
 void rotate(float degrees) {
 	printf("Rotating to a number of degrees");
-	current_angle = fmod(gyro_get_degrees(),360);
+	current_angle = fmod(get_heading(),360);
 	printf("start angle = %f \n", current_angle);
 	float desired_angle = current_angle + degrees;
 	printf("desired_angle = %f \n", desired_angle);
@@ -51,46 +53,72 @@ void rotate(float degrees) {
 	}
 	else {
 		if (degrees>0) {
-			set_velocities(150, -150);
-			while (fmod(gyro_get_degrees(),360) < desired_angle) {
-				printf("current angle = %f \n", fmod(gyro_get_degrees(),360));
+			set_velocities(SPEED, -SPEED);
+			while (fmod(get_heading(),360) < desired_angle) {
+				printf("current angle = %f \n", fmod(get_heading(),360));
 			}
 		}
 		else {
-			set_velocities(-150, 150);
-			while (fmod(gyro_get_degrees(),360) > desired_angle) {
-				printf("current angle = %f \n", fmod(gyro_get_degrees(),360));
+			set_velocities(-SPEED, SPEED);
+			while (fmod(get_heading(),360) > desired_angle) {
+				printf("current angle = %f \n", fmod(get_heading(),360));
 			}
 		}
 		brake();
 	}
-	current_angle = gyro_get_degrees();
 }
 
-void move_to(struct Point p, float velocity) {
-	//moves in a straight line to the desired point at the desired velocity
-	current_angle = gyro_get_degrees();
-	float dist_x = p.x - current_loc.x;
-	float dist_y = p.y - current_loc.y;
-	float dist = sqrt(pow(dist_x, 2)+pow(dist_y, 2));
-	float desired_angle = atan(dist_y/dist_x);
+// void move_to(Point p, float velocity) {
+// 	//moves in a straight line to the desired point at the desired velocity
+// 	current_angle = gyro_get_degrees();
+// 	float dist_x = p.x - current_loc.x;
+// 	float dist_y = p.y - current_loc.y;
+// 	float dist = sqrt(pow(dist_x, 2)+pow(dist_y, 2));
+// 	float desired_angle = atan(dist_y/dist_x);
 	
-	rotate(desired_angle-current_angle);
-	set_velocity(velocity);
+// 	rotate(desired_angle-current_angle);
+// 	set_velocity(velocity);
 
-	while (dist > DRIVE_TOLERANCE) {
-		update_location();
-		dist_x = p.x - current_loc.x;
-		dist_y = p.y - current_loc.y;
-		dist = sqrt(pow(dist_x, 2)+pow(dist_y, 2));
-		pause(100);
-	}
-	brake();
-}
+// 	while (dist > DRIVE_TOLERANCE) {
+// 		update_location();
+// 		dist_x = p.x - current_loc.x;
+// 		dist_y = p.y - current_loc.y;
+// 		dist = sqrt(pow(dist_x, 2)+pow(dist_y, 2));
+// 		pause(100);
+// 	}
+// 	brake();
+// }
 
 void move_for_time(float velocity, float millis) {
 	printf("Moving for a certain amount of time");
 	set_velocity(velocity);
 	pause(millis);
 	brake();
+}
+
+void dumb_moving() {
+	float dist_x;
+	float dist_y;
+	float dist;
+	float desired_angle;
+	float angle_threshold;
+	while (1) {
+		dist_x = active_target.x - current_loc.x;
+		dist_y = active_target.y - current_loc.y;
+		dist = sqrt(pow(dist_x, 2)+pow(dist_y, 2));
+		desired_angle = atan(dist_y/dist_x);
+		current_angle = get_heading();
+		angle_threshold = 1000/dist; //this should be tweaked
+		if(abs(current_angle-desired_angle)>angle_threshold) {
+			brake();
+			rotate(desired_angle-current_angle);
+		}
+		set_velocity(SPEED);
+		pause(300);
+	}
+}
+
+float get_heading() {
+	//do some fancy averaging thing instead
+	return gyro_get_degrees();
 }

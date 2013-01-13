64884c7d4f4c5fd26c77a1ebf860200270c87f79
Jessica Kenney <seveneightn9ne@gmail.com>
2013-01-12 22:29:25 -0500

    fixed errors

--- src/inc/encoder_utils.h
+++ src/inc/encoder_utils.h
@@ -1,13 +1,16 @@
-uint16_t encoder_distance(uint16_t read_diff = 0) {
+uint16_t encoder_distance_diff(uint16_t read_diff) {
 	return 	(  155 // small wheel circumfrence in mm  
 			 * (1./25.) // gear ratio 
-			 * (read_diff or encoder_read(ENCODER_PIN)) 
+			 * read_diff 
 			 / 6) ;
+}
+uint16_t encoder_distance() {
+	return encoder_distance_diff(encoder_read(ENCODER_PIN));
 }
 uint16_t encoder_speed() {
 	//mm/s according to 100ms sample
 	uint16_t first_read = encoder_read(ENCODER_PIN);
 	pause(100);
 	uint16_t second_read = encoder_read(ENCODER_PIN);
-	return encoder_distance(second_read - first_read) * 10;
+	return encoder_distance_diff(second_read - first_read) * 10;
 }

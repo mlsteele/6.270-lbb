86d764c8df5ca023e0022bd3d5b47823c5bb023f
Miles Steele <miles@milessteele.com>
2013-01-14 00:52:00 -0500

    add hw_config entries

--- user/robot/inc/hw_config.h
+++ user/robot/inc/hw_config.h
@@ -6,17 +6,19 @@
 // PINS
 #define PIN_MOTOR_DRIVE_L 0
 #define PIN_MOTOR_DRIVE_R 1
+#warning define gyro_pins
+#define PIN_GYRO 21
 
-#error please have the common courtesy of defining your encoder pins!
-#define PIN_ENCODER_WHEEL_L
-#define PIN_ENCODER_WHEEL_R
+// #error please have the common courtesy of defining your encoder pins!
+// #define PIN_ENCODER_WHEEL_L
+// #define PIN_ENCODER_WHEEL_R
 
 
 // PHYSICAL
+#warning these physical constant definitions are notoriously uninformed
 #define MM_WHEEL_FROM_CENTER 50
-#error MM_PER_TICK_WHEELS not defined
-#define MM_PER_TICK_WHEELS
-
+#define MM_PER_TICK_WHEELS 4
+#define LSB_US_PER_DEG 1386583
 
 // CONVERSION
 #define DEGS_TO_RADS 0.01745328f

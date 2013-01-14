fc6efa1f0c391f3b14bdd0f5c11fd152d07c3625
Miles Steele <miles@milessteele.com>
2013-01-14 17:56:55 -0500

    add more printing to movement_calibration_test

--- user/robot/steele/movement_calibration_umain.c
+++ user/robot/steele/movement_calibration_umain.c
@@ -12,7 +12,7 @@ int umain (void) {
   printf("print encs_snapshot\n");
   // encoder_reset(PIN_ENCODER_WHEEL_L);
   // encoder_reset(PIN_ENCODER_WHEEL_R);
-  // printf("encs_snapshot [%i : %i]\n", encoder_read(PIN_ENCODER_WHEEL_L), encoder_read(PIN_ENCODER_WHEEL_R));
+  printf("encs_snapshot [%i : %i]\n", encoder_read(PIN_ENCODER_WHEEL_L), encoder_read(PIN_ENCODER_WHEEL_R));
 
   printf("move seq\n");
 
@@ -37,16 +37,17 @@ int umain (void) {
   float pow_chain[11] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1};
 
   for(int i = 0; i < pow_chain_len; i++) {
+    printf("encs_snapshot [%i : %i]\n", encoder_read(PIN_ENCODER_WHEEL_L), encoder_read(PIN_ENCODER_WHEEL_R));
     printf("setting wheel pows to %f\n", pow_chain[i]);
     set_wheel_pows(pow_chain[i], pow_chain[i]);
-    pause(150);
+    pause(250);
   }
 
   printf("stopping...");
   set_wheel_pows(0, 0);
   printf(" done");
   
-  // printf("encs_snapshot [%i : %i]\n", encoder_read(PIN_ENCODER_WHEEL_L), encoder_read(PIN_ENCODER_WHEEL_R));
+  printf("encs_snapshot [%i : %i]\n", encoder_read(PIN_ENCODER_WHEEL_L), encoder_read(PIN_ENCODER_WHEEL_R));
 
   return 0;
 }

aeea75e3ed27a00fe052d8d5dd1cb0e826e26392
Miles Steele <miles@milessteele.com>
2013-01-14 15:12:22 -0500

    fix movement_calibration (hack)

--- user/robot/steele/movement_calibration_umain.c
+++ user/robot/steele/movement_calibration_umain.c
@@ -14,20 +14,32 @@ int umain (void) {
   // encoder_reset(PIN_ENCODER_WHEEL_R);
   // printf("encs_snapshot [%i : %i]\n", encoder_read(PIN_ENCODER_WHEEL_L), encoder_read(PIN_ENCODER_WHEEL_R));
 
-  printf("power and enter move seq\n");
-  float com_pow = 0;
-  float pow_targ = 0.6;
-  float duration = 1000;
-  set_wheel_pows(com_pow, com_pow);
-  for (; com_pow < pow_targ; com_pow += pow_targ / (duration / 10)) {
-    printf("setting wheels to %f\n", com_pow);
-    set_wheel_pows(com_pow, com_pow);
-    pause(10);
-  }
-  for (; com_pow < pow_targ; com_pow += pow_targ / (duration / 10)) {
-    printf("setting wheels to %f\n", com_pow);
-    set_wheel_pows(com_pow, com_pow);
-    pause(10);
+  printf("move seq\n");
+
+  // float com_pow = 0;
+  // float pow_targ = 0.6;
+  // float duration = 1000;
+  // set_wheel_pows(com_pow, com_pow);
+  // for (; com_pow < pow_targ; com_pow += pow_targ / (duration / 10)) {
+  //   printf("setting wheels to %f\n", com_pow);
+  //   set_wheel_pows(com_pow, com_pow);
+  //   pause(10);
+  // }
+  // pause(30);
+  // com_pow = pow_targ;
+  // for (; com_pow > 0; com_pow -= pow_targ / (duration / 10)) {
+  //   printf("setting wheels to %f\n", com_pow);
+  //   set_wheel_pows(com_pow, com_pow);
+  //   pause(10);
+  // }
+
+  int pow_chain_len = 11;
+  float pow_chain[11] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1};
+
+  for(int i = 0; i < pow_chain_len; i++) {
+    printf("setting wheel pows to %f\n", pow_chain[i]);
+    set_wheel_pows(pow_chain[i], pow_chain[i]);
+    pause(150);
   }
 
   printf("stopping...");

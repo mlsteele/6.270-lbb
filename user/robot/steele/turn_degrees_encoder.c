e84ba2220217b4ce3fd9cdc467851f9c7b3a50d1
Miles Steele <miles@milessteele.com>
2013-01-14 23:33:37 -0500

    add physical calibration for encoders, add broken turn_degrees_encoder

---
+++ user/robot/steele/turn_degrees_encoder.c
@@ -0,0 +1,28 @@
+#include <joyos.h>
+#include <hw_config.h>
+#include <moving.h>
+
+int usetup() {
+  gyro_init(PIN_GYRO, LSB_US_PER_DEG, 500);
+
+  return 0;
+}
+
+float read_rotational_encoding() {
+  return (encoder_read(PIN_ENCODER_WHEEL_L) + encoder_read(PIN_ENCODER_WHEEL_R)) * MM_PER_TICK_WHEELS;
+}
+
+int umain() {
+  encoder_reset(PIN_ENCODER_WHEEL_L);
+  encoder_reset(PIN_ENCODER_WHEEL_R);
+
+  set_wheel_pows(0.3, -0.3);
+  while(read_rotational_encoding() < 4 * M_PI * MM_WHEEL_FROM_CENTER) {
+    printf("cirtr: %f\n", read_rotational_encoding());
+  }
+
+  printf("stopping...");
+  set_wheel_pows(0, 0);
+  printf(" done\n");  
+
+}

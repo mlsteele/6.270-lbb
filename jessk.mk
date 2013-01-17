e98703d6fd0148552355194660ed21ea7b9e3cdf
Jessica Kenney <seveneightn9ne@gmail.com>
2013-01-17 18:55:45 -0500

    Cleaned up my vps code

---
+++ jessk.mk
@@ -0,0 +1,9 @@
+# User source files
+USERSRC = user/robot/cat_mouse/cat_mouse_main.c
+
+# Serial port
+AVRDUDE_USERPORT ?= COM6
+
+# Mac Users - you may need to change this to stk500 if you get an error when programming
+PROGRAMMER = stk500v1
+
--- user/robot/jessk/vps_navigation_umain.c
+++ user/robot/jessk/vps_navigation_umain.c
@@ -6,8 +6,6 @@
 #include "get_vps.h"
 #include "vps_utils.h"
 
-//#include "../cat_mouse/move_towards_target_smooth.h"
-
 int usetup (void) {
   extern volatile uint8_t robot_id;
   robot_id = 8;
@@ -16,7 +14,18 @@ int usetup (void) {
 }
 
 int umain (void) {
+	while(1) {
 
+		Point target = get_vps_target();
+		printf("Got new target (%.2f, %.2f)\n", target.x, target.y);
+		vps_rotate(target_theta(target));
+		vps_drive_towards(target);
+
+		/*while(1) {
+			vps_dump();
+			pause(500);
+		}*/
+	}
     return 0;
 }
 

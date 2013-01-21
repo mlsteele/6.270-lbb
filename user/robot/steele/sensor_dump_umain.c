dbd348c5a34085f78ee7e1091abafdaad15d198e
Miles Steele <miles@milessteele.com>
2013-01-21 15:21:59 -0500

    add wheels to sensor dump

--- user/robot/steele/sensor_dump_umain.c
+++ user/robot/steele/sensor_dump_umain.c
@@ -9,12 +9,18 @@ int usetup (void) {
 }
 
 int umain (void) {
+  printf("testing wheels forward...\n");
+  set_wheel_pows(0.3, 0.3);
+  pause(50);
+  set_wheel_pows(0, 0);
+  printf("done...\n");
+
   printf("preparing to print sensors...\n");
 
   while(1) {
     printf("gyro: %f", gyro_get_degrees());
-    printf("enc_l: %i", encoder_read(PIN_ENCODER_WHEEL_L));
-    printf("enc_r: %i", encoder_read(PIN_ENCODER_WHEEL_R));
+    printf("  enc_l: %i", encoder_read(PIN_ENCODER_WHEEL_L));
+    printf("  enc_r: %i", encoder_read(PIN_ENCODER_WHEEL_R));
     printf("\n");
     pause(50);
   }

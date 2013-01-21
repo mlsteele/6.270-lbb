0fd083c31821d18048a2d448ef6d7c8b0d459b38
Jessica Kenney <seveneightn9ne@gmail.com>
2013-01-21 00:24:02 -0500

    generalized move_to_territory() for any territory UNTESTED

--- user/robot/jessk/umain_move_to_territory.c
+++ user/robot/jessk/umain_move_to_territory.c
@@ -9,6 +9,24 @@
 
 Point territories[6];
 
+void move_to_territory(uint8_t territory){
+	//Pick a path 
+	int dir;
+	if((territory-current_territory())%6 <= 3) 
+		 {dir =  1;} 
+	else {dir = -1;}
+
+	//move into the target territory
+	while(territory-current_territory() != 0){ 
+		vps_aim_towards_target(territories[(current_territory()+dir)%6]);
+	}
+	//move to center of current territory
+	while(find_distance(territories[target_territory])>200){
+		vps_aim_towards_target(territories[current_territory()]);
+	}
+	set_wheel_pows(0,0);
+}
+
 void move_to_next_territory(){
 	uint8_t target_territory = (current_territory()+1)%6;
 	while(find_distance(territories[target_territory])>200){

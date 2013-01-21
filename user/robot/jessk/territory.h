c9db6b835565c01c07de403bd8853f778bd91675
Jessica Kenney <seveneightn9ne@gmail.com>
2013-01-21 00:35:22 -0500

    cleaned up and reorganized territory stuff

--- user/robot/jessk/territory.h
+++ user/robot/jessk/territory.h
@@ -14,3 +14,31 @@ uint8_t territory_of_point(Point p) {
 uint8_t current_territory() {
 	return territory_of_point(get_vps_current());
 }
+
+void move_to_territory(uint8_t territory){
+	//Pick a path 
+	//TODO include enemy in calculations of path
+	int dir;
+	if((territory-current_territory())%6 <= 3) 
+		 {dir =  1;} 
+	else {dir = -1;}
+
+	//move into the target territory
+	while(territory-current_territory() != 0){ 
+		vps_aim_towards_target(territories[(current_territory()+dir)%6]);
+	}
+	//move to center of current territory
+	while(find_distance(territories[target_territory])>200){
+		vps_aim_towards_target(territories[current_territory()]);
+	}
+	set_wheel_pows(0,0);
+}
+
+void move_to_next_territory(){
+	uint8_t target_territory = (current_territory()+1)%6;
+	while(find_distance(territories[target_territory])>200){
+		printf("\t%.2f away...\n", find_distance(territories[target_territory]));
+		vps_aim_towards_target(territories[target_territory]);
+	}
+	set_wheel_pows(0,0);
+}
